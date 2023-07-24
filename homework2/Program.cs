582cdab96d4466875981d5c0391cf827fed1a435
Cigfan <anisenyst@yandex.ru>
2023-07-24 21:08:41 +0300

    homework2

--- homework2/Program.cs
+++ homework2/Program.cs
@@ -3,23 +3,38 @@
 782 -> 8
 918 -> 1*/
 
-Console.Write("Введите Ваше число: ");
-int Decimus = int.Parse(Console.ReadLine()!);
-
-
+Console.Write("Введите трезначное число: ");
+int trio = int.Parse(Console.ReadLine()!);
+string triostring = trio.ToString();
+if(triostring.Length == 3){
+    Console.WriteLine(triostring[1]);
+}
+else Console.WriteLine("Число не корректно.");
 
 /*Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 645 -> 5
 78 -> третьей цифры нет
 32679 -> 6*/
 
-Console.Write("Введите Ваше число: ");
-int Trius = int.Parse(Console.ReadLine()!);
-if(Trius >= 100){
-    
+Console.Write("Введите трезначное число: ");
+int quad = int.Parse(Console.ReadLine()!);
+string quadstring = quad.ToString();
+if(quadstring.Length == 3){
+    Console.WriteLine(quadstring[2]);
 }
+else Console.WriteLine($"В числе {quadstring} третьей цифры нет.");
 
-/*Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+/*Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, 
+является ли этот день выходным.
 6 -> да
 7 -> да
 1 -> нет*/
+
+Console.Write("Введите номер дня недели: ");
+int week = int.Parse(Console.ReadLine()!);
+if (week > 0 && week < 8)
+{
+    string result = (week == 6 || week == 7 ? "Выходной" : "Рабочий день");
+    Console.WriteLine(result);
+}
+else Console.WriteLine("Вы ввели некорректный день недели.");
--- lesson2/Program.cs
+++ lesson2/Program.cs
@@ -1,2 +1,69 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*int num = new Random().Next(1000);
+string numstring = num.ToString();
+
+if(numstring.Length == 3){
+    Console.WriteLine($"Число {num} без второй цифры имеет вид {numstring[0]}{numstring[2]}");
+}
+else{
+    Console.WriteLine("Число не трехзначное");
+    }
+*/
+// Задача 1. Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+// 78 -> 8
+// 12-> 2
+// 85 -> 8
+
+// int first = new Random().Next(10, 100);
+// string numstring = first.ToString();
+// if(numstring[0] > numstring[1]){
+//     Console.WriteLine($"{numstring[0]} больше чем {numstring[1]} в числе {first}");
+// }
+// else{
+//     Console.WriteLine($"{numstring[1]} больше чем {numstring[0]} в числе {first}");
+// }
+
+/*Задача 2. Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому. 
+Если второе число не кратно числу первому, то программа выводит остаток от деления.
+
+34, 5 -> не кратно, остаток 4
+16, 4  -> кратно*/
+
+/*Console.Write("Введите первое число: ");
+int second1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int second2 = int.Parse(Console.ReadLine()!);
+if(second1 % second2 == 0){
+    Console.WriteLine("Да, кратно первому числу.");
+}
+else{
+    Console.WriteLine($"Нет, не кратно, остаток от деления - {second1 % second2}");
+}*/
+
+/*Задача 3. Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+14  ->  нет
+46  ->  нет
+161 ->  да*/
+
+// Console.Write("Введите первое число: ");
+// int Alpha = int.Parse(Console.ReadLine()!);
+// if(Alpha % 7 == 0 && Alpha % 23 == 0){
+//     Console.WriteLine("Да");
+// }
+// else{
+//     Console.WriteLine("нет");
+// }
+
+/*Задача 4. Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.*/
+
+Console.Write("Введите первое число: ");
+int hitotsu = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int futatsu = int.Parse(Console.ReadLine()!);
+if(hitotsu * hitotsu == futatsu){
+    Console.WriteLine($"Число {futatsu} является квадратом числа {hitotsu}");
+}
+else if(futatsu * futatsu == hitotsu){
+    Console.WriteLine($"Число {hitotsu} является квадратом числа {futatsu}");
+}
+else{Console.WriteLine($"ниодно из чисел {hitotsu} и {futatsu} не является квадратом другого.");}
+
