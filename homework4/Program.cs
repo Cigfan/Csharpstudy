fd874b56fc160da01c12286af7484978304eb015
Cigfan <anisenyst@yandex.ru>
2023-07-31 22:09:52 +0300

    lesson4

---
+++ homework4/Program.cs
@@ -0,0 +1,32 @@
+﻿/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16*/
+
+// Console.Write("Введите первое число: ");
+// int first = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите второе число: ");
+// int second = int.Parse(Console.ReadLine()!);
+// int power = first;
+// for(int i = 1; i < second; i++){
+//     first *= power;
+// }
+// Console.Write($"Число {power} в степени {second} равно {first}.");
+
+
+/*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+
+// Console.Write("Введите Ваше число: ");
+// int raw = int.Parse(Console.ReadLine()!);
+// string cooked = raw.ToString();
+// int sum = 0;
+// for(int i = 0; i < cooked.Length; i++){
+//     sum += Convert.ToInt32(cooked[i]);
+// }
+// Console.Write($"{sum}");
+
+/*Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. (можно указать любой промежуток)
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]*/
---
+++ lesson4/Program.cs
@@ -0,0 +1,69 @@
+﻿// Console.Write("Введите Ваше число А: ");
+// int A = int.Parse(Console.ReadLine()!);
+// Console.WriteLine($"Сумма чисел от 1 до {A} равна {GetSum(A)}");
+
+// //Методы
+// int GetSum(int A){
+//     int sum = 0;
+//     for(int i = 1; i <= A; i++){
+//         sum += i;
+//     }
+//     return sum;
+// }
+
+// Задача 1: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+
+// 4  -> 24
+// 5  -> 120
+
+// Console.Write("Введите Ваше число: ");
+// int omega = int.Parse(Console.ReadLine()!);
+// Console.WriteLine($"Произведения чисел от 1 до {omega} равна {MyMethod(omega)}");
+
+// //method
+// int MyMethod(int omega) {
+//     int sum = 1;
+//     for(int i = 1; i <= omega; i++){
+//         sum *= i;
+//     }
+//     return sum;
+// }
+
+// Задача 2: Определить, является ли заданное шестизначное число счастливым. 
+// (Счастливым называют такое шестизначное число, что сумма его первых трех цифр равна сумме его 
+// последних трех цифр, порядок чисел в сумме не важен). Решить самым оптимальным способом.
+
+Console.WriteLine("Введите шестизначное число: ");
+int N = int.Parse(Console.ReadLine()!);
+if(N >= 100000 && N <= 999999){
+    if (LuckyNumber(N) == true) Console.WriteLine($"Число {N} - счастливое");
+    else Console.WriteLine($"Число {N} - не счастливое");
+}
+else Console.WriteLine("Некорректный ввод");
+
+bool LuckyNumber(int num){
+    string number = num.ToString();
+    int first = (Convert.ToInt32(number[0]) + Convert.ToInt32(number[1]) + Convert.ToInt32(number[2]));
+    int second = (Convert.ToInt32(number[3]) + Convert.ToInt32(number[4]) + Convert.ToInt32(number[5]));
+
+    if (first == second){
+        return true;
+    }
+    return false;
+}
+
+// Задача 3: Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+
+// [1,0,1,1,0,1,0,0]
+
+int[] array = GetRange(8);
+Console.WriteLine($"[{String.Join(",", array)}]");
+
+//method
+int[] GetRange(int size){
+    int[] massive = new int[size];
+    for(int i = 0; i < massive.Length; i++){
+        massive[i] = new Random().Next(2);
+    }
+    return massive;
+}
